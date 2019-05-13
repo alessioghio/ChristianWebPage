a1d64cea3fa6ffa0baaa86448239655356d54c92
Christian Ledgard <christianledgard@gmail.com>
2019-05-12 22:40:00 -0500

    Revert "minor updates"
    This reverts commit 76ed6ee4df311aaf88a938f6cfce66d0ab5725e0.

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
-import os
 from flask import Flask, render_template, session, redirect, request, url_for
-
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY','XYZ')
-
-
-def connect_db():
-    return psycopg2.connect(os.environ.get('DATABASE_URL'))
-
 
 @app.route("/")
 
